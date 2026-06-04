a25e7fbac9b8803f4506a11501728103b2b7d3fd
Macaukit <100654385+Macaukit@users.noreply.github.com>
2026-06-04 13:07:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-const CACHE_NAME = 'pingpong-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'pingpong-v3';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/']))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// 永遠從網路取得最新資料，不用快取
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
